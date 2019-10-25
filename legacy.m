ead5638dc6c6444f03c35de925c0f155e58b9911
DimitriDeSchuyter <dimitri.deschuyter@gmail.com>
2019-10-25 13:44:14 +0200

    input option to execute parts of code

--- legacy.m
+++ legacy.m
@@ -20,7 +20,7 @@ m = m.connect();
 m.list
 
 %% Declaration of measurement
-id = 58;
+id = input('ID: ');
 if( size(find(m.list.id == id),1)==1 && m.list.count(find(m.list.id == id,1)) > 2)
     disp('ID exists, running...')
     m=m.set_measurement_ID(id);
@@ -37,60 +37,67 @@ else
 end
 
 %% Export data to workspace
-m.exportData();
-
+export = input('export to workspace (Y/N): ','s');
+if  export== "y" || export == "Y" || export == "yes" 
+    m.exportData();
+end
 
 %% Plot all measurement information
-close all;
- 
-display(['Moment of measurement: ' datestr(datetime(m.start_time, 'convertfrom','posixtime'),'yyyy-mm-dd HH:MM:SS.FFF')]);
 
-if  exist('id','var')
-    if  m.list.count(find(m.list.id == id,1)) > 2
-        m.plot_all();
-    elseif m.list.count < 2
-        disp("measurement contains no data")
+plotting = input('plot the measurement (Y/N): ','s');
+if  plotting== "y" || plotting == "Y" || plotting == "yes" 
+    close all;
+    display(['Moment of measurement: ' datestr(datetime(m.start_time, 'convertfrom','posixtime'),'yyyy-mm-dd HH:MM:SS.FFF')]);
+
+    if  exist('id','var')
+        if  m.list.count(find(m.list.id == id,1)) > 2
+            m.plot_all();
+        elseif m.list.count < 2
+            disp("measurement contains no data")
+        else
+            disp("ID does not exist in database")
+        end
     else
-        disp("ID does not exist in database")
+        disp("get first the data")
     end
-else
-    disp("get first the data")
+   
 end
-
 
 %% save workspace to .mat file in data folder
-StoreName = 'filename'; 
-if ~exist('data', 'dir')
-    mkdir('data')
+SaveWorkspace = input('Save workspace to .mat (Y/N): ','s');
+if  SaveWorkspace== "y" || SaveWorkspace == "Y" || SaveWorkspace == "yes" 
+    StoreName = 'filename'; 
+    if ~exist('data', 'dir')
+        mkdir('data')
+    end
+    store = fullfile(pwd, 'data', [StoreName '.mat']);
+    save(store);  
 end
-store = fullfile(pwd, 'data', [StoreName '.mat']);
-save(store);
-
 
 %% Own analysis: type your own commands
- 
-time = (1:size(m.instruments(1,2).data(1,6).values,2))*0.02 + m.start_time;
-figure();
-fontsize = 20;
-set(gca,'fontsize',fontsize) % set fontsize of the plot to 20
-set(gcf, 'Position', get(0,'Screensize')); % automatic full screen
-set(0, 'DefaultAxesFontSize', fontsize);
-plot(time,m.instruments(1,1).data(1,2).values,'LineWidth',2)
-ylabel("Turn",'fontsize',fontsize);
-ax1 = gca;
-yyaxis right
-plot(time, m.instruments(1,2).data(1,6).values,'LineWidth',2)
-xlabel('Time [s]','fontsize',fontsize);
-ylabel("gz",'fontsize',fontsize);
-xlim(([min(time) (max(time))]));
-title("gz vs turn" ,'fontsize',fontsize);
-legend(["turn","gz"]);
+ownCode = input('Execute own code (Y/N): ','s');
+if  ownCode== "y" || ownCode == "Y" || ownCode == "yes" 
+    time = (1:size(m.instruments(1,2).data(1,6).values,2))*0.02 + m.start_time;
+    figure();
+    fontsize = 20;
+    set(gca,'fontsize',fontsize) % set fontsize of the plot to 20
+    set(gcf, 'Position', get(0,'Screensize')); % automatic full screen
+    set(0, 'DefaultAxesFontSize', fontsize);
+    plot(time,m.instruments(1,1).data(1,2).values,'LineWidth',2)
+    ylabel("Turn",'fontsize',fontsize);
+    ax1 = gca;
+    yyaxis right
+    plot(time, m.instruments(1,2).data(1,6).values,'LineWidth',2)
+    xlabel('Time [s]','fontsize',fontsize);
+    ylabel("gz",'fontsize',fontsize);
+    xlim(([min(time) (max(time))]));
+    title("gz vs turn" ,'fontsize',fontsize);
+    legend(["turn","gz"]);
 
-%% Heatmaps
-% figure()
-% heatmap([m.instruments(1,1).data(1,2).values m.instruments(1,2).data(1,6).values])
-
-
+    %% Heatmaps
+    % figure()
+    % heatmap([m.instruments(1,1).data(1,2).values m.instruments(1,2).data(1,6).values])
+end
 
 
 
